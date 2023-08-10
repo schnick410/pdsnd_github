0e2c80373b0d2d3ce38e8ad52b0b6081d281561a
Niklas Schniedermeier <niklas.schniedermeier@gmail.com>
2023-08-10 11:06:22 +0200

    abbreviate daynames to increase reading speed

--- bikeshare_Progress.py
+++ bikeshare_Progress.py
@@ -18,7 +18,7 @@ DAY_DATA = [0, 1, 2, 3, 4, 5, 6, 7]
 
 """ List to group day number to day name """
 
-DAY_GROUPING = {0: 'Monday', 1: 'Tuesday', 2: 'Wednesday', 3: 'Thursday', 4: 'Friday', 5: 'Saturday', 6: 'Sunday', 7: 'no selection'}
+DAY_GROUPING = {0: 'Mon.', 1: 'Tue.', 2: 'Wed.', 3: 'Thu.', 4: 'Fri.', 5: 'Sat.', 6: 'Sun.', 7: 'no selection'}
 
 def get_filters():
     """
